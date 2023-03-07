448c7a54ba1215ee02432920c753790255e01155
Dastanbek <muktaraliuuludastanbek@gmail.com>
2023-03-07 11:46:50 +0600

    fist commit

---
+++ src/Models/EatAndDrinkDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EatAndDrink.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class EatAndDrinkDbContext : DbContext
+    {
+        // TODO: Нужно обновить строку подключения
+        // Вместо <сервер> указываем имя сервера SQL Server Expres
+        // Вместо <пароль> указываем пароль созданный при установке SQL Server Expres
+        private readonly String _connectionString = "Data Source=LAPTOP-J753BN4M\\SQLEXPRESS;Initial Catalog=EatAndDrinkDB;Persist Security Info=True;TrustServerCertificate=True;Encrypt=False;User ID = sa;Password=123;";
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(_connectionString);
+    }
+}
