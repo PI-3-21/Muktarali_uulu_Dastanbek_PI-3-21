878b9ac431b85514fcacb9a50ec83f58b31dd1bc
Dastanbek <muktaraliuuludastanbek@gmail.com>
2023-03-10 15:57:50 +0600

    4-laba_first_commit

--- src/Migrations/20230307055613_2-7.Designer.cs
+++ src/Migrations/20230307055613_2-7.Designer.cs
@@ -3,6 +3,7 @@ using EatAndDrink.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EatAndDrink.Migrations
 {
     [DbContext(typeof(EatAndDrinkDbContext))]
-    partial class EatAndDrinkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230307055613_2-7")]
+    partial class _27
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/Models/EatAndDrinkDbContext.cs
+++ src/Models/EatAndDrinkDbContext.cs
@@ -16,6 +16,14 @@ namespace EatAndDrink.Models
         // Вместо <пароль> указываем пароль созданный при установке SQL Server Expres
         private readonly String _connectionString = "Data Source=LAPTOP-J753BN4M\\SQLEXPRESS;Initial Catalog=EatAndDrinkDB;Persist Security Info=True;TrustServerCertificate=True;Encrypt=False;User ID = sa;Password=123;";
         public DbSet<Person> Persons { get; set; }
+        public DbSet<Courier> Couriers { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Restorant> Restorants { get; set; }
+
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(_connectionString);
     }
